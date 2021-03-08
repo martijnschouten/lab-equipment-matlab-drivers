270bfb749f8c11aa34753d87fddd6fe0defe9ec3
martijnschouten <martijn-schouten@hotmail.com>
2021-03-08 11:45:42 +0100

    fixed bug that made that the smac couldn't be set to zero force

--- SMAC actuator/examples/mechanical spectrum analyser/SMAC_read_and_set_Force.m
+++ SMAC actuator/examples/mechanical spectrum analyser/SMAC_read_and_set_Force.m
@@ -1,6 +1,10 @@
 function [position,force] = SMAC_read_and_set_Force(actuator,force)
-if abs(force) <= 1000 
-    fprintf(actuator, ['32 W 0x006071 ' num2str(int32(force),'%3.0d')]);%set force
+if abs(force) <= 1000
+    if force ~= 0
+        fprintf(actuator,['32 W 0x006071 ' num2str(int32(force),'%3.0d')])
+    else
+        fprintf(actuator,['32 W 0x006071 ' num2str(0)])
+    end
 else
     error('force is out of range')
 end
